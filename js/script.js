6576cbd2ec1c6e733607e166c9cd298f28df4aa0
Felipe Álvarez <falvarez@votainteligente.cl>
2014-01-21 16:29:01 -0300

    Removing countdown and changin subtitle

--- js/script.js
+++ js/script.js
@@ -10,27 +10,5 @@ $(function(){
 		ts = (new Date(2014,0,21,16,0,0));
 		newYear = true;
 	}
-		
-	$('#countdown').countdown({
-		timestamp	: ts,
-		callback	: function(days, hours, minutes, seconds){
-			
-			var message = "";
-			
-			message += days + " día" + ( days==1 ? '':'s' ) + ", ";
-			message += hours + " hora" + ( hours==1 ? '':'s' ) + ", ";
-			message += minutes + " minuto" + ( minutes==1 ? '':'s' ) + " y ";
-			message += seconds + " segundo" + ( seconds==1 ? '':'s' ) + " ";
-			
-			if(newYear){
-				message += "para el lanzamiento de votainteligente.cl!";
-			}
-			else {
-				message += "para el lanzamiento de votainteligente.cl!";
-			}
-			
-			note.html(message);
-		}
-	});
 	
 });
