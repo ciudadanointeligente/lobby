1e9ae84bc1feec63900b30fe493ffcba130b208f
Pedro Daire <pdaire@gmail.com>
2014-08-29 13:10:22 -0400

    agrega cuenta regresova para entrada en vigencia

--- js/script.js
+++ js/script.js
@@ -7,8 +7,33 @@ $(function(){
 	if((new Date()) > ts){
 		// The new year is here! Count towards something else.
 		// Notice the *1000 at the end - time must be in milliseconds
-		ts = (new Date(2014,0,21,16,0,0));
+		ts = (new Date(2014,10,28,8,0,0));
 		newYear = true;
-	}
+	};
+
+
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " día" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hora" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minuto" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " segundo" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			if(newYear){
+				message += "para la entrada en vigencia de la ley del lobby";
+			}
+			else {
+				message += "para la entrada en vigencia de la ley del lobby2";
+			}
+			
+			note.html(message);
+		}
+	});
+
+
 	
 });
